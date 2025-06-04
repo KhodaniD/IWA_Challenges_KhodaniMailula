343bd1288c026775922fa080b769b2bd9cbf26da
Khodani Mailula <khodanid@gmail.com>
2025-06-04 15:49:46 +0200

    Fixed Challenge2-d issues

--- Challenge 2/Challenge d/script.js
+++ Challenge 2/Challenge d/script.js
@@ -1,5 +1,12 @@
-import company form 'configuration'
-import year form 'configuration'
 
-const message = '© ' + company + ' (' + year + ')'
-document.querySelector('footer').innerText = message
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn('Security scan starting');
+
+//t is important to let user know when they can close the page 
+console.info('Please wait for scan to complete before closing the browser.');
